1ceb07f87399f932d4949e64235a156e98c63aaa
Katherine <katherine.pineault@gmail.com>
2016-10-16 18:48:48 -0400

    Tous les exercices du Chapitre 3

--- ExercicesChap3.R
+++ ExercicesChap3.R
@@ -67,4 +67,52 @@ rm(x1,x2,x3,x4,y1,y2,y3,y4,factorielle)
 rm(Constante_dEuler)                
 ## FIN DE LA CONSTANTE D'EULER--------------------------------------------------------------
         
+## Les suites arithmético-géométriques
+        # Une suite arithmético-géométrique de valeur initiale u0 et de paramètres a et b est définie
+        # par la relation de récurrence. U0=u0, U(n+1)=aUn +b
+        #Écrire une fonction qui prend en premier paramètre l'entier n, puis les valeurs de a, b et u0
+        # en mettant pour valeurs par défaut: a=1, b=0 et u0=1
+
+suite_a_g<- function(n, a=1, b=0, u0=1){
+        suite<- as.vector(NULL)
+        U_n<- u0
+        for(i in 1:n){
+                
+           U_n<- (a*U_n) +b     
+        suite<- rbind(suite, U_n[1])        
+        }
+        suite[,1]        
+        }
+
+suite_a_g(n=10, a=2, b=1, u0=2)
+
+rm(suite_a_g)
+## FIN DE LES SUITES ARITHMÉTICO-GÉOMÉTRIQUES-----------------------------------------------
+
+## La suite de Fibonacci 
+        #La suite de Fibonacci de valeur initiale u0 et u1 est définie par la relation de récurrence
+        # U0=u0
+        # U1=u1
+        # Un+2= Un+1 + Un
         
+Fibonnaci<- function(n,u0=0,u1=1){
+        x<- as.vector(c(u1))
+        y<- u0
+        z<- u1
+        for(i in 1:n){
+                w<- y+z
+                y<-z
+                z<- w
+                x<-cbind(x,w[1])
+        }
+        
+        
+        x[1,]
+}
+
+Fibonnaci(10)
+Fibonnaci(25, u0=3, u1=4)
+
+
+rm(Fibonnaci)
+### FIN DES EXERCICES DU CHAPITRE 3
